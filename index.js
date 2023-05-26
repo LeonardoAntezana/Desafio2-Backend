fafdb385c485694bb628137679cb0f76798de038
¨Leonardo <leonardoantezana59@gmail.com>
2023-05-26 10:39:35 -0300

    creando repositorio local

---
+++ index.js
@@ -0,0 +1,22 @@
+import { ProductManager } from "./ProductManager.js";
+
+// const man1 = new ProductManager('leonardo', './prueba.json');
+
+// const main = async () => {
+//   const call1 =  await man1.getProducts();
+//   console.log(call1)
+//   await man1.addProduct('producto prueba', 0, 2000, 'Sin imagen', 'ABC123', 25);
+//   await man1.addProduct('producto prueba', 'este es un producto de prueba', 2000, 'Sin imagen', 'ABC', 25);
+//   const call3 =  await man1.getProducts();
+//   console.log(call3)
+//   const prodFind = await man1.getProductById(0)
+//   console.log(`El resultado del objeto a buscar : ${prodFind.id}`)
+//   await man1.updateProduct(0, 'id', 500);
+//   const call4 = await man1.getProducts();
+//   console.log(call4)
+//   await man1.deleteProduct(1);
+//   const call5 = await man1.getProducts();
+//   console.log(call5)
+// }
+
+// main();
